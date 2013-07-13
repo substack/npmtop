33cd2e5c5ef67df4eaab76f843f47e5b40e59c10
nickleefly <nickleefly@gmail.com>
2013-07-14 00:08:53 +0800

    use http JSON.parse(body)
    update readme, add travis

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var uri = 'https://isaacs.iriscouch.com/registry/'
+var uri = 'http://isaacs.iriscouch.com/registry/'
     + '_design/app/_view/npmTop?group_level=1';
 
 module.exports = function (cb) {
@@ -30,11 +30,11 @@ module.exports = function (cb) {
 };
 
 function getScores (cb) {
-    request({ uri : uri, json : true }, function (err, res, body) {
+    request({ uri : uri}, function (err, res, body) {
         if (err) return cb(err)
         if (body.error) return cb(body.error)
-        
-        var scores = body.rows.reduce(function (acc, row) {
+
+        var scores = JSON.parse(body).rows.reduce(function (acc, row) {
             acc[row.key] = row.value;
             return acc;
         }, {});
